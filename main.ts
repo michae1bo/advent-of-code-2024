35d0b23dfb41f3ad8009fc761ade14cd8620e0b7
michae1bo <159921466+michae1bo@users.noreply.github.com>
2024-12-06 18:18:32 +0100

    Day 6

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { dayTwoA, dayTwoB } from "./day-2.ts";
 import { dayThreeA, dayThreeB } from "./day-3.ts";
 import { dayFourA, dayFourB } from "./day-4.ts";
 import { dayFiveA, dayFiveB } from "./day-5.ts";
+import { daySixA, daySixB } from "./day-6.ts";
 
 if (import.meta.main) {
     console.log("(Day 1)  Part 1:", await dayOneA(), "\n         Part 2:", await dayOneB());
@@ -10,4 +11,5 @@ if (import.meta.main) {
     console.log("(Day 3)  Part 1:", await dayThreeA(), "\n         Part 2:", await dayThreeB());
     console.log("(Day 4)  Part 1:", await dayFourA(), "\n         Part 2:", await dayFourB());
     console.log("(Day 5)  Part 1:", await dayFiveA(), "\n         Part 2:", await dayFiveB());
+    console.log("(Day 6)  Part 1:", await daySixA(), "\n         Part 2:", await daySixB());
   }
