9339c8de40b04ddb0a1b8e7e12f54ad5883278c1
michae1bo <159921466+michae1bo@users.noreply.github.com>
2024-12-07 15:38:27 +0100

    Day 7

---
+++ day-7.ts
@@ -0,0 +1,65 @@
+export async function daySevenA(): Promise<number> {
+    let totalCalibrationResult: number = 0;
+    const text: string = await Deno.readTextFile("day-7.input");
+    const textRows: string[] = text.split(/\n/);
+    textRows.forEach((row: string) => {
+        const numbers: number[] = row.split(/(?:\s)/).map((numberString: string): number => parseInt(numberString));
+        if (performOperation("*", numbers) || performOperation("+", numbers)) {
+            totalCalibrationResult += numbers[0];
+        }
+        
+    })
+    
+    return totalCalibrationResult;
+} 
+
+export async function daySevenB(): Promise<number> {
+    let totalCalibrationResult: number = 0;
+    const text: string = await Deno.readTextFile("day-7.input");
+    const textRows: string[] = text.split(/\n/);
+    textRows.forEach((row: string) => {
+        const numbers: number[] = row.split(/(?:\s)/).map((numberString: string): number => parseInt(numberString));
+        if (performOperationB("*", numbers) || performOperationB("+", numbers) || performOperationB("||", numbers)) {
+            totalCalibrationResult += numbers[0];
+        }
+        
+    })
+    
+    return totalCalibrationResult;
+}
+
+function performOperation(operator: string, numbers: number[]): boolean {
+    if (numbers.length === 2) {
+            return numbers[0] === numbers[1];
+    } else {
+        let result: number = 0;
+        if (operator === "+") {
+            result = numbers[1] + numbers[2];
+        } else {
+            result = numbers[1] * numbers[2];
+        }
+        return performOperation("*", [numbers[0], result].concat(numbers.slice(3))) || performOperation("+", [numbers[0], result].concat(numbers.slice(3)));
+    }
+}
+
+function performOperationB(operator: string, numbers: number[]): boolean {
+    if (numbers.length === 2) {
+            return numbers[0] === numbers[1];
+    } else {
+        let result: number = 0;
+        if (operator === "+") {
+            result = numbers[1] + numbers[2];
+        } else if (operator === "*"){
+            result = numbers[1] * numbers[2];
+        } else {
+            result = parseInt(numbers[1].toString() + numbers[2].toString());
+        }
+        return performOperationB("*", [numbers[0], result].concat(numbers.slice(3))) || performOperationB("+", [numbers[0], result].concat(numbers.slice(3))) || performOperationB("||", [numbers[0], result].concat(numbers.slice(3)));
+    }
+}
+
+
+if (import.meta.main) {
+    console.log("Part 1:", await daySevenA());
+    console.log("Part 2:", await daySevenB());
+}
--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import { dayThreeA, dayThreeB } from "./day-3.ts";
 import { dayFourA, dayFourB } from "./day-4.ts";
 import { dayFiveA, dayFiveB } from "./day-5.ts";
 import { daySixA, daySixB } from "./day-6.ts";
+import { daySevenA, daySevenB } from "./day-7.ts";
 
 if (import.meta.main) {
     console.log("(Day 1)  Part 1:", await dayOneA(), "\n         Part 2:", await dayOneB());
@@ -12,4 +13,5 @@ if (import.meta.main) {
     console.log("(Day 4)  Part 1:", await dayFourA(), "\n         Part 2:", await dayFourB());
     console.log("(Day 5)  Part 1:", await dayFiveA(), "\n         Part 2:", await dayFiveB());
     console.log("(Day 6)  Part 1:", await daySixA(), "\n         Part 2:", await daySixB());
+    console.log("(Day 7)  Part 1:", await daySevenA(), "\n         Part 2:", await daySevenB());
   }
